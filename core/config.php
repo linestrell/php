8803a5734c3c205a3a39b417d0366776b412b051
linestrell <linestrell@gmail.com>
2019-11-22 17:50:58 +0500

    lesson7

---
+++ core/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "shop");
+define("CATALOG_IMAGES_PATH", "img/catalog/");
+define("DEFAULT_ADMIN", "catalog");
+define("DEFAULT_MOD", "catalog");
